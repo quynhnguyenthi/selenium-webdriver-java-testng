5f252adbf8d1d28057cb2ad9dcb92b7ba737f0cc
Quynh Nguyen <quynhnguyen.ftu2@gmail.com>
2021-09-12 12:26:12 +0700

    Topic 11 - done TC08

--- selenium/webdriver/Topic_11_User_Interaction.java
+++ selenium/webdriver/Topic_11_User_Interaction.java
@@ -7,10 +7,12 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -65,24 +67,82 @@ public class Topic_11_User_Interaction {
 		WebElement subWatches = driver.findElement(By.xpath("//a[@class='desktop-categoryLink' and text()='Watches']"));
 		subWatches.click();
 		sleep(3);
-		Assert.assertTrue(driver.findElement(By.xpath("//span[@class='breadcrumbs-crumb' and text()='Kids Watches']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@class='breadcrumbs-crumb' and text()='Kids Watches']"))
+				.isDisplayed());
 		Assert.assertEquals(driver.getCurrentUrl(), "https://www.myntra.com/kids-watches");
 	}
-	
+
 //	@Test
 	public void TC_03_Hover() throws InterruptedException {
 		driver.get("https://www.fahasa.com/");
 		sleep(3);
-		WebElement menu = driver.findElement(By.xpath("//div[@class=\"col-md-3 hidden-max-width-992\"]//span[@class='menu-title' and text()='Sách Trong Nước']"));
+		WebElement menu = driver.findElement(By.xpath(
+				"//div[@class=\"col-md-3 hidden-max-width-992\"]//span[@class='menu-title' and text()='Sách Trong Nước']"));
 		action.moveToElement(menu).perform();
-		WebElement subMenu = driver.findElement(By.xpath("//div[contains(@class,'col-md-3 hidden-max-width-992')]//a[contains(text(),'Nhân Vật - Bài Học Kinh Doanh')]"));
+		WebElement subMenu = driver.findElement(By.xpath(
+				"//div[contains(@class,'col-md-3 hidden-max-width-992')]//a[contains(text(),'Nhân Vật - Bài Học Kinh Doanh')]"));
 		Assert.assertTrue(subMenu.isDisplayed());
-		
 	}
 
-	@Test
+//	@Test
 	public void TC_04_Click_Hold() {
-		
+		driver.get("https://automationfc.github.io/jquery-selectable/");
+		List<WebElement> listItems = driver.findElements(By.xpath("//li"));
+		action.clickAndHold(listItems.get(0)).moveToElement(listItems.get(3)).release().perform();
+		List<WebElement> listItemsSelected = driver.findElements(By.cssSelector("li[class*='ui-selected']"));
+		System.out.println(listItemsSelected.size());
+		Assert.assertEquals(4, listItemsSelected.size());
+	}
+	
+//	@Test
+	public void TC_05_Click_multiple_random() {;
+		driver.get("https://automationfc.github.io/jquery-selectable/");
+		List<WebElement> listItems = driver.findElements(By.xpath("//li"));
+		action.keyDown(Keys.CONTROL).perform();
+//		1 - 3 - 6 - 11
+		listItems.get(0).click();
+		listItems.get(2).click();
+		listItems.get(5).click();
+		listItems.get(10).click();
+		List<WebElement> listItemsSelected = driver.findElements(By.cssSelector("li[class*='ui-selected']"));
+		System.out.println(listItemsSelected.size());
+		Assert.assertEquals(4, listItemsSelected.size());
+	}
+//	@Test
+	public void TC_06_double_click() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		WebElement doubleClickButton = driver.findElement(By.xpath("//button[text()='Double click me']"));
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", doubleClickButton);
+		action.doubleClick(doubleClickButton).perform();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@id='demo' and text()='Hello Automation Guys!']")).isDisplayed());
+	}
+	
+//	@Test
+	public void TC_07_right_click() throws InterruptedException {
+		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+		WebElement buttonToRightClick = driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+		action.contextClick(buttonToRightClick).perform();
+		Assert.assertFalse(driver.findElement(By.cssSelector("li[class*='context-menu-icon-quit'][class*='context-menu-hover'][class*='context-menu-visible']")).isDisplayed());
+		action.moveToElement(driver.findElement(By.cssSelector("li[class*='icon-quit']"))).perform();
+		Assert.assertTrue(driver.findElement(By.cssSelector("li[class*='context-menu-icon-quit'][class*='context-menu-hover'][class*='context-menu-visible']")).isDisplayed());
+		driver.findElement(By.cssSelector("li[class*='icon-quit']")).click();
+		Assert.assertEquals(driver.switchTo().alert().getText(), "clicked: quit");
+		driver.switchTo().alert().accept();
+		Assert.assertFalse(driver.findElement(By.cssSelector("li[class*='context-menu-icon-quit'][class*='context-menu-hover'][class*='context-menu-visible']")).isDisplayed());
+	} 
+	
+	@Test
+	public void TC_8_drag_drop() throws InterruptedException {
+		driver.get("https://automationfc.github.io/kendo-drag-drop/");
+		WebElement small = driver.findElement(By.id("draggable"));
+		WebElement big = driver.findElement(By.id("droptarget"));
+		action.dragAndDrop(small, big).perform();
+		sleep(3);
+		Assert.assertEquals(big.getText(), "You did great!");
+		Color loginButtonBackgroundColour = Color.fromString(big.getCssValue("background-color"));
+		System.out.println(loginButtonBackgroundColour);
+		System.out.println(loginButtonBackgroundColour.asHex());
+		Assert.assertEquals(loginButtonBackgroundColour.asHex(), "#03a9f4");
 	}
 	@AfterTest
 	public void afterTest() {
